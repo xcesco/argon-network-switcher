9c63db6770845c96ccc870a3bc86c0c5bb664983
xcesco@gmail.com <xcesco@gmail.com@d439d89f-f001-07c8-e71e-dade250d534f>
2011-08-09 09:44:08 +0000

    work in progress

--- Argon/trunk/ArgonNetworkSwitcher/FormAdapters.Designer.cs
+++ Argon/trunk/ArgonNetworkSwitcher/FormAdapters.Designer.cs
@@ -29,21 +29,36 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAdapters));
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // FormAdapters
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HideOnClose = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormAdapters";
+            this.TabText = "Network Adapters";
             this.Text = "Network Adapters";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
--- Argon/trunk/ArgonNetworkSwitcher/FormMain.cs
+++ Argon/trunk/ArgonNetworkSwitcher/FormMain.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using WeifenLuo.WinFormsUI.Docking;
+using System.IO;
 
 namespace Argon
 {
@@ -36,6 +37,41 @@ namespace Argon
                 return Controller.Instance.ViewProfiles;
             }           */
             return null;
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string configFile = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "DockPanel.config");
+
+           dockPanel.SaveAsXml(configFile);
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            string configFile = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "DockPanel.config");
+
+            FormAdapters formAdapters = new FormAdapters();
+            formAdapters.Show(dockPanel);
+            formAdapters.DockState = DockState.DockLeft;
+
+            /*
+            if (File.Exists(configFile))
+            {
+                dockPanel.LoadFromXml(configFile, _DeserializeDockContent);
+            }
+            else*/
+          /*  {
+                Controller.Instance.ViewProfiles.Show(dockPanel);
+                Controller.Instance.ViewProfiles.DockState = DockState.DockLeft;
+                Controller.Instance.ViewAdapters.Show(dockPanel);
+                Controller.Instance.ViewAdapters.DockState = DockState.DockLeft;
+
+                mnuViewNetworkAdapters.Checked = true;
+                mnuViewProfiles.Checked = true;
+            }
+
+            Controller.Instance.Init();*/
+
         }
     }
 }
