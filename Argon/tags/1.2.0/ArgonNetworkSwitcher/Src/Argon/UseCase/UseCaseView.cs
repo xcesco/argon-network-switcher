fd03c17ebef7e02dc2c8292687e05b70338d4ddc
xcesco@gmail.com <xcesco@gmail.com@d439d89f-f001-07c8-e71e-dade250d534f>
2012-02-27 21:58:47 +0000

    tag of version 1.2.0

---
+++ Argon/tags/1.2.0/ArgonNetworkSwitcher/Src/Argon/UseCase/UseCaseView.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Argon.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+using Argon.Models;
+using System.Windows.Forms;
+using Argon.Windows.Network.Profile;
+
+/*
+ * Copyright 2012 Francesco Benincasa
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+namespace Argon.UseCase
+{
+    public abstract class UseCaseView
+    {
+        /// <summary>
+        /// Toggles the display.
+        /// </summary>
+        /// <param name="dockContent">Content of the dock.</param>
+        public static void ToggleDisplay(ArgonDockContent dockContent)
+        {
+            if (dockContent.Visible)
+            {
+                // hide content
+                Hide(dockContent);
+            }
+            else
+            {
+                // display content
+                Display(dockContent, true, dockContent.OldDockState);
+            }
+        }
+
+        /// <summary>
+        /// Displays the specified dock content.
+        /// </summary>
+        /// <param name="dockContent">Content of the dock.</param>
+        /// <param name="dockState">State of the dock.</param>
+        /// <returns></returns>
+        public static bool Display(ArgonDockContent dockContent, bool forceShow=false,DockState dockState=DockState.Unknown)
+        {
+            if (forceShow)
+            {
+                dockContent.DockState = dockState;
+
+                if (dockState == DockState.Unknown && dockContent.OldDockState != DockState.Unknown)
+                {
+                    dockContent.DockState = dockContent.OldDockState;
+                }
+
+                dockContent.Show();
+            }
+
+            // check ribbon button
+            if (dockContent == ViewModel.ProfilesView)
+            {                
+                CheckRibbonButton(dockContent, ViewModel.MainView.rbtnViewProfiles);
+            }
+            else if (dockContent == ViewModel.NetworkCardsView)
+            {
+                CheckRibbonButton(dockContent, ViewModel.MainView.rbtnViewNetworkCards);
+            }
+            else if (dockContent == ViewModel.ConsoleView)
+            {
+                CheckRibbonButton(dockContent, ViewModel.MainView.rbtnViewConsole);
+            }                                    
+
+            return true;
+        }
+
+        /// <summary>
+        /// Displays the specified dock content.
+        /// </summary>
+        /// <param name="dockContent">Content of the dock.</param>
+        /// <param name="dockState">State of the dock.</param>
+        /// <returns></returns>
+        public static void Hide(ArgonDockContent dockContent)
+        {            
+            dockContent.OldDockState = dockContent.DockState;
+            dockContent.DockState = DockState.DockBottom;
+            dockContent.Hide();
+
+            // check ribbon button
+            if (dockContent == ViewModel.ProfilesView)
+            {
+                CheckRibbonButton(dockContent, ViewModel.MainView.rbtnViewProfiles);
+            }
+            else if (dockContent == ViewModel.NetworkCardsView)
+            {
+                CheckRibbonButton(dockContent, ViewModel.MainView.rbtnViewNetworkCards);
+            }
+            else if (dockContent == ViewModel.ConsoleView)
+            {
+                CheckRibbonButton(dockContent, ViewModel.MainView.rbtnViewConsole);
+            }
+            
+        }
+
+        /// <summary>
+        /// Checks the ribbon button.
+        /// </summary>
+        /// <param name="dockContent">Content of the dock.</param>
+        /// <param name="button">The button.</param>
+        /// <returns></returns>
+        internal static void CheckRibbonButton(ArgonDockContent dockContent, RibbonButton button)
+        {
+            //ActionRefreshNetworkAdapters();
+            if (dockContent.Visible)
+            {
+                button.Checked = true;
+            }
+            else
+            {
+                button.Checked = false;
+            }
+        }
+
+
+        /// <summary>
+        /// Activates the form cards.
+        /// </summary>
+        public static void ActivateFormCards()
+        {
+            FormMain main = ViewModel.MainView;
+
+            // form selezionati
+            ViewModel.SelectedView = ViewModel.NetworkCardsView;
+        }
+
+        /// <summary>
+        /// Activates the form network card.
+        /// </summary>
+        /// <param name="selectedNetworkCardForm">The selected network card form.</param>
+        public static void ActivateFormNetworkCard(FormNetworkCard selectedNetworkCardForm)
+        {/*
+            // profili
+            ViewModel.MainView.rbtnProfileNew.Enabled = false;
+            ViewModel.MainView.rbtnProfileView.Enabled = false;
+            ViewModel.MainView.rbtnProfileDelete.Enabled = false;
+
+            // profilo
+            ViewModel.MainView.rbtnProfileRun.Enabled = false;
+            ViewModel.MainView.rbtnProfileSave.Enabled = false;
+
+            // documento
+            ViewModel.MainView.rbtnConfigSave.Enabled = false;
+            ViewModel.MainView.rbtnConfigLoad.Enabled = false;
+
+            // networkcard
+            ViewModel.MainView.rbtnCardsRefresh.Enabled = false;
+            ViewModel.MainView.rbtnCardView.Enabled = false;*/
+
+            ViewModel.MainView.ribbon.ActiveTab = ViewModel.MainView.rtOperations;
+
+            // form selezionati            
+            ViewModel.SelectedView = selectedNetworkCardForm;
+        }
+
+        /// <summary>
+        /// Activates the form network card.
+        /// </summary>
+        /// <param name="selectedNetworkCardForm">The selected network card form.</param>
+        public static void ActivateFormProfile(FormProfile selectedProfileForm)
+        {
+            /*
+            // profili
+            ViewModel.MainView.rbtnProfileNew.Enabled = true;
+            ViewModel.MainView.rbtnProfileView.Enabled = true;
+            ViewModel.MainView.rbtnProfileDelete.Enabled = true;
+
+            // profilo
+            ViewModel.MainView.rbtnProfileRun.Enabled = true;
+            ViewModel.MainView.rbtnProfileSave.Enabled = true;
+
+            // documento
+            ViewModel.MainView.rbtnConfigSave.Enabled = true;
+            ViewModel.MainView.rbtnConfigLoad.Enabled = true;
+
+            // networkcard
+            ViewModel.MainView.rbtnCardsRefresh.Enabled = false;
+            ViewModel.MainView.rbtnCardView.Enabled = false;*/
+
+            ViewModel.MainView.ribbon.ActiveTab = ViewModel.MainView.rtOperations;
+
+            // form selezionati            
+            ViewModel.SelectedView = selectedProfileForm;
+        }
+
+        /// <summary>
+        /// Shows the specified profile.
+        /// </summary>
+        /// <param name="profile">The profile.</param>
+        public static void ShowProfile(NetworkProfile profile)
+        {
+            if (profile == null) return;
+
+            NetworkProfile item;
+
+            foreach (FormProfile itemForm in ViewModel.ProfileViewList)
+            {
+                if ((itemForm.Tag is NetworkProfile))
+                {
+                    item = (NetworkProfile)itemForm.Tag;
+
+                    if (item.Id.Equals(profile.Id))
+                    {
+                        if (!itemForm.Visible)
+                        {
+                            itemForm.Show(ViewModel.MainView.Pannello);
+                        }
+
+                        itemForm.Focus();
+                        //UseCaseView.ActivateFormProfile(itemForm);
+                        return;
+                    }
+                }
+            }            
+
+            FormProfile form = new FormProfile();
+            form.Profile = profile;
+            form.ViewDataOnForm();
+
+            ViewModel.ProfileViewList.Add(form);
+            form.Show(ViewModel.MainView.Pannello);
+            form.DockState = DockState.Document;            
+        }
+
+
+
+        /// <summary>
+        /// Shows the new profile.
+        /// </summary>
+        public static void ShowNewProfile()
+        {
+            NetworkProfile profile = new NetworkProfile();
+            profile.Name = UseCaseProfile.NEW_NIC_NAME;
+
+            ShowProfile(profile);
+        }
+
+        /// <summary>
+        /// Finds the and close profile.
+        /// </summary>
+        /// <param name="profile">The profile.</param>
+        public static bool FindAndCloseProfile(NetworkProfile profile)
+        {
+            foreach (FormProfile form in ViewModel.ProfileViewList)
+            {
+                if (form.Profile.Id.Equals(profile.Id))
+                {
+                    form.Close();
+                    ViewModel.ProfileViewList.Remove(form);
+                    return true;
+                }
+
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Clears the console.
+        /// </summary>
+        public static void ClearConsole()
+        {
+            ViewModel.ConsoleView.lstBox.Items.Clear();
+        }
+    }
+}
---
+++ Argon/tags/1.2.0/ArgonUnitTest/Src/Argon/Network/AutoDetectTest.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Argon.OperatingSystem;
+using System.IO;
+using System.Net.NetworkInformation;
+using Argon.Windows.Network.Profile;
+
+namespace Argon.Windows.Network
+{
+    [TestClass]
+    public class AutoDetectTest
+    {
+
+        /// <summary>
+        /// Setups the profiles.
+        /// </summary>
+        /// <returns></returns>
+        public List<NetworkProfile> SetupProfiles()
+        {
+            List<NetworkProfile> list = new List<NetworkProfile>();
+
+            string file = Path.GetFullPath(@"..\..\..\ArgonUnitTest\TestProfiles.xml");
+
+            Console.WriteLine(file);
+            Assert.IsTrue(File.Exists(file));
+
+            list = NetworkProfileHelper.Load(file,"");
+
+
+            // string relPath = System.IO.Path.Combine(Application.StartupPath, "TestProfiles.xml"); 
+
+
+            return list;
+        }
+
+        [TestMethod]
+        public void TestPing()
+        {
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+
+            // Use the default Ttl value which is 128,
+            // but change the fragmentation behavior.
+            options.DontFragment = true;
+
+            // Create a buffer of 32 bytes of data to be transmitted.
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 120;
+            PingReply reply = pingSender.Send("172.22.8.2", timeout, buffer, options);
+            if (reply.Status == IPStatus.Success)
+            {
+                Console.WriteLine("Address: {0}", reply.Address.ToString());
+                Console.WriteLine("RoundTrip time: {0}", reply.RoundtripTime);
+                Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
+                Console.WriteLine("Don't fragment: {0}", reply.Options.DontFragment);
+                Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
+            }
+        }
+
+        /// <summary>
+        /// Tests the auto detect.
+        /// </summary>
+        [TestMethod]
+        public void TestAutoDetect()
+        {
+            Console.WriteLine("Start");
+
+            // load test profiles
+            List<NetworkProfile> networkProfileList = SetupProfiles();
+
+            NetworkProfile current=NetworkProfileHelper.AutoDetectNetworkProfile(networkProfileList);
+
+
+            if (current != null) { Console.WriteLine("Profile name: {0}, Wifi SSID: {1} ", current.Name, current.AssociatedWifiSSID); }
+            else
+            {
+                Console.WriteLine("No profile found");
+            }
+
+            Console.WriteLine("Finished!");
+        }
+
+        [TestMethod]
+        public void ShowNetworkInterfaces()
+        {
+            IPGlobalProperties computerProperties = IPGlobalProperties.GetIPGlobalProperties();
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            Console.WriteLine("Interface information for {0}.{1}     ",
+                    computerProperties.HostName, computerProperties.DomainName);
+            if (nics == null || nics.Length < 1)
+            {
+                Console.WriteLine("  No network interfaces found.");
+                return;
+            }
+
+            Console.WriteLine("  Number of interfaces .................... : {0}", nics.Length);
+            foreach (NetworkInterface adapter in nics)
+            {
+                IPInterfaceProperties properties = adapter.GetIPProperties();
+                Console.WriteLine();
+                Console.WriteLine(adapter.Description);
+                Console.WriteLine(String.Empty.PadLeft(adapter.Description.Length, '='));
+                Console.WriteLine("  Interface type .......................... : {0}", adapter.NetworkInterfaceType);
+                Console.WriteLine("  Physical Address ........................ : {0}",
+                           adapter.GetPhysicalAddress().ToString());
+                Console.WriteLine("  Operational status ...................... : {0}",
+                    adapter.OperationalStatus);
+                string versions = "";
+
+                // Create a display string for the supported IP versions.
+                if (adapter.Supports(NetworkInterfaceComponent.IPv4))
+                {
+                    versions = "IPv4";
+                }
+                if (adapter.Supports(NetworkInterfaceComponent.IPv6))
+                {
+                    if (versions.Length > 0)
+                    {
+                        versions += " ";
+                    }
+                    versions += "IPv6";
+                }
+                Console.WriteLine("  IP version .............................. : {0}", versions);
+                //ShowIPAddresses(properties);
+
+                // The following information is not useful for loopback adapters.
+                if (adapter.NetworkInterfaceType == NetworkInterfaceType.Loopback)
+                {
+                    continue;
+                }
+                Console.WriteLine("  DNS suffix .............................. : {0}",
+                    properties.DnsSuffix);
+                
+                if (adapter.Supports(NetworkInterfaceComponent.IPv4))
+                {
+                    IPv4InterfaceProperties ipv4 = properties.GetIPv4Properties();
+                    Console.WriteLine("  MTU...................................... : {0}", ipv4.Mtu);
+                    if (ipv4.UsesWins)
+                    {
+
+                        IPAddressCollection winsServers = properties.WinsServersAddresses;
+                        if (winsServers.Count > 0)
+                        {
+                           // string label;
+                            //label = "  WINS Servers ............................ :";
+                            // ShowIPAddresses(label, winsServers);
+                        }
+                    }
+                }
+
+                Console.WriteLine("  DNS enabled ............................. : {0}",
+                    properties.IsDnsEnabled);
+                Console.WriteLine("  Dynamically configured DNS .............. : {0}",
+                    properties.IsDynamicDnsEnabled);
+                Console.WriteLine("  Receive Only ............................ : {0}",
+                    adapter.IsReceiveOnly);
+                Console.WriteLine("  Multicast ............................... : {0}",
+                    adapter.SupportsMulticast);
+                // ShowInterfaceStatistics(adapter);
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
