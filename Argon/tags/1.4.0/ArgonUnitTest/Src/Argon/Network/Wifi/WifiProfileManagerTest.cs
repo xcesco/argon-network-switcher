ea413d53aad1ae2af7e699c18c19d0c2f9054eee
xcesco@gmail.com <xcesco@gmail.com@d439d89f-f001-07c8-e71e-dade250d534f>
2012-05-11 22:46:36 +0000

    release 1.4.0

---
+++ Argon/tags/1.4.0/ArgonUnitTest/Src/Argon/Network/Wifi/WifiProfileManagerTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Argon.Windows.Network.Wifi;
+
+namespace Argon.Network.Wifi
+{
+    [TestClass]
+    public class WifiProfileManagerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+            List<WifiProfile> WifiProfiles=WifiProfileManager.WifiProfiles;
+
+            foreach (WifiProfile item in WifiProfiles)
+            {
+                Console.WriteLine(item.SSID + " " + item.InterfaceName);
+            }
+        }
+    }
+}
