3445670ff9e5cf89fcf55a0ae63a6ff8cbc55e68
xcesco@gmail.com <xcesco@gmail.com@d439d89f-f001-07c8-e71e-dade250d534f>
2014-11-01 16:50:40 +0000

    release 1.4.8

---
+++ Argon/tags/1.4.8/ArgonNetworkSwitcher/Src/Argon/Windows/Controls/DNSConfiguration.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Argon.Windows.Network;
+
+/*
+ * Copyright 2012 Francesco Benincasa
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+namespace Argon.Windows.Controls
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [DesignTimeVisible(true), ToolboxItem(true),
+  ToolboxBitmap(typeof(Button))]
+    public partial class DNSConfiguration : UserControl
+    {
+        public DNSConfiguration()
+        {
+            // impostiamo il modo trasparente prima di inizializzare
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            BackColor = Color.Transparent;
+
+            InitializeComponent();
+
+            // to avoid flicker problem
+            // see http://stackoverflow.com/questions/64272/how-to-eliminate-flicker-in-windows-forms-custom-control-when-scrolling
+            SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                        ControlStyles.UserPaint |
+                        ControlStyles.AllPaintingInWmPaint, true);
+            
+        }
+
+        /// <summary>
+        /// Displays the configuration.
+        /// </summary>
+        public void DisplayConfiguration(WindowsNetworkCard config)
+        {
+            if (config == null)
+            {                
+                cbDynamicDNS.Checked = true;
+                return;
+            }
+
+            cbDynamicDNS.Checked = config.DynamicDNS;
+            txtPrimaryDNS.IpAddress = config.Dns;
+            txtAlternativeDNS.IpAddress = config.Dns2;
+        }
+
+        /// <summary>
+        /// Saves the configuration.
+        /// </summary>
+        public void SaveConfiguration(WindowsNetworkCard config)
+        {
+            if (config == null) return;
+
+            config.DynamicDNS = cbDynamicDNS.Checked;
+            config.Dns = txtPrimaryDNS.IpAddress;
+            config.Dns2 = txtAlternativeDNS.IpAddress;
+        }
+
+        private void cbDynamicDNS_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbDynamicDNS.Checked)
+            {
+                txtPrimaryDNS.Enabled = false;
+                txtAlternativeDNS.Enabled = false;
+            }
+            else
+            {
+                txtPrimaryDNS.Enabled = true;
+                txtAlternativeDNS.Enabled = true;
+            }
+        }
+    }
+
+    
+       
+}
---
+++ Argon/tags/1.4.8/ArgonNetworkSwitcher/Src/Argon/Windows/Controls/MACAddressControl.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Argon.Windows.Controls
+{
+    partial class MACAddressControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtMacAddress = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtMacAddress);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(376, 60);
+            this.groupBox1.TabIndex = 25;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mac Address";
+            // 
+            // txtMacAddress
+            // 
+            this.txtMacAddress.Location = new System.Drawing.Point(25, 23);
+            this.txtMacAddress.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMacAddress.Name = "txtMacAddress";
+            this.txtMacAddress.ReadOnly = true;
+            this.txtMacAddress.Size = new System.Drawing.Size(312, 22);
+            this.txtMacAddress.TabIndex = 0;
+            this.txtMacAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MACAddress
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MACAddress";
+            this.Size = new System.Drawing.Size(376, 60);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtMacAddress;
+    }
+}
